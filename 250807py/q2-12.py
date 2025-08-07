619841260122143c0312c8aec29c27558fa7dc35
sbg0629 <sbg0629@naver.com>
2025-08-07 19:26:04 +0900

    키보드로 입력받고 결제 금액 출력하기

---
+++ 250807py/q2-12.py
@@ -0,0 +1,11 @@
+a = int(input("책 값 은? "))
+b = int(input("할인율 은? "))
+c = int(input("배송료 은? "))
+d = a - (a*(b/100))+c
+print("책값은? ",a)
+print("할인율? ",b)
+print("배송료? ",c)
+print("결제금액 %d 원" %d)
+
+
+
