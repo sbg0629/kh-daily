6d3363bd5e67e7f3ba36f6663c74e993410a94b1
sbg0629 <sbg0629@naver.com>
2025-08-08 18:38:27 +0900

    5의 배수 합계

---
+++ 250808/ex4-6.py
@@ -0,0 +1,13 @@
+sum = 0
+
+# for i in range(100,201,5):
+#     sum +=i
+#     print("5의 배수 합계: %d"%sum) 
+#     #출력문도 반복
+# print("5의 배수 합계: %d"%sum) #스페이스 없어도 정상 출력
+#마지막 결과 값만 출력
+
+for i in range(100,201):
+    if i%5 ==0:
+        sum +=i
+print("5의 배수의 합계: %d"%sum)
