6e3a9b9f78813819868ddaf7765a0c345b217175
sbg0629 <sbg0629@naver.com>
2025-08-08 19:17:41 +0900

    if문 등급

---
+++ 250808/q3-3.py
@@ -0,0 +1,22 @@
+g = input("등급(대문자)을 입력하세요")
+
+if g == "A+":
+    print("등급:%s,평점은 4.5" %g)
+if g == "A":
+    print("등급:%s,평점은 4.0" %g)
+if g == "B+":
+    print("등급:%s,평점은 3.5" %g)
+if g == "B":
+    print("등급:%s,평점은 3.0" %g)
+if g == "C+":
+    print("등급:%s,평점은 2.5" %g)
+if g == "C":
+    print("등급:%s,평점은 2.0" %g)
+if g == "D+":
+    print("등급:%s,평점은 1.5" %g)
+if g == "D":
+    print("등급:%s,평점은 1.0" %g)
+if g == "F":
+    print("등급:%s,평점은 0" %g)
+else:
+    print("등급을 잘못 입력 했습니다.")
