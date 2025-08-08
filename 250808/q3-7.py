83e91ef9add82eba5737d5f5c28dc9da79728b8b
sbg0629 <sbg0629@naver.com>
2025-08-08 19:18:51 +0900

    대문자로도 자음 모음

---
+++ 250808/q3-7.py
@@ -0,0 +1,15 @@
+a = input("영문 소문자 하나를 입력하세요")
+# b = a.upper() 
+
+if a == "a" or a == "e" or a == "i" or a == "o" or a == "u":
+    print("%s은 모음 입니다."%a)
+
+# if b == "A" or b == "E" or b == "I" or b == "O" or b == "U":
+    # print("%s은 모음 입니다."%a) upper 쓸 대.
+    
+# elif a == "A" or a == "E" or a == "I" or a == "O" or a == "U":
+#     print("%s은 모음 입니다."%a)
+else:
+    print("%s은 자음 입니다."%a)
+    
+    
