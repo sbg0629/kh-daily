9a9dda20f25275eac407c99b6e19ecb3a77fd973
sbg0629 <sbg0629@naver.com>
2025-08-08 19:18:22 +0900

    가장 큰 값 구하는 문제

---
+++ 250808/q3-5.py
@@ -0,0 +1,18 @@
+a = int(input("첫번쨰 정수를 입력하세요"))
+b = int(input("두번째 정수를 입력하세요"))
+c = int(input("세번째 정수를 입력하세요"))
+
+d = max(a,b,c) #가장 큰 값 비교
+    #min 은 작은 값
+print("%d %d %d 중 가장 큰 값은 %d" %(a,b,c,d))
+
+# if a > b and a > 3:
+#     lagest = a #이런 느낌임
+# elif b > a and b > c:
+#     lagest = b
+# else:
+#     lagest = c
+
+# print("%d %d %d 중 가장 큰 값은 %d" %(a,b,c,lagest))
+
+# ㄴ 이렇게도 가능하다.
