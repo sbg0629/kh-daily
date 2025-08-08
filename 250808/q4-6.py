9b39cf893d9730b9a97b9a08b6d20ccc6bd55fce
sbg0629 <sbg0629@naver.com>
2025-08-08 19:24:54 +0900

    3의 배수가 아니면 출력함

---
+++ 250808/q4-6.py
@@ -0,0 +1,17 @@
+i = 1
+sum = 0
+count = 0
+while i <= 1000:
+    if i % 3 != 0:
+        print(i ,end=" ")
+        
+        count += 1
+        if count %10 ==0:
+            print()
+        
+    i+=1
+
+
+
+
+
