dc95122f0caef588d996adc69a46b1b65898b097
sbg0629 <sbg0629@naver.com>
2025-08-08 19:26:28 +0900

    별을 5개씩 10개 출력

---
+++ 250808/q4-10.py
@@ -0,0 +1,11 @@
+# for i in range(0,5):
+#      # for j in range(0,5):
+#     print("* "*10)
+    
+for i in range(5):
+    for j in range(10):
+        print("*",end=" ")
+    print()
+   
+        
+    
