2eb597a107866ac581bc1c95b8c6efa40adbc8b9
sbg0629 <sbg0629@naver.com>
2025-08-08 19:17:28 +0900

    if elif 문 봄 여름 가을 겨울

---
+++ 250808/q3-2.py
@@ -0,0 +1,23 @@
+num = int(input("숫자를 입력하세요 :"))
+
+if 3<=num<=5:
+    print("%d은 봄이다" %num)
+elif 6<=num<=8:
+    print("%d은 여름이다" %num)
+elif 9<=num<=11:
+    print("%d은 가을이다" %num)
+
+else:
+    print("%d은 겨울이다" %num)
+
+# mon = input("월을 입력하세요")
+# if mon =="3"or mon=="4"or mon=="5":
+#     print("%s 월은 봄입니다" %mon)
+# elif mon =="6" or mon =="7" or mon =="8":
+#     print("%s 월은 여름입니다" %mon)
+# elif mon =="9" or mon =="10" or mon =="11":
+#     print("%s 월은 가을입니다" %mon)
+# else:
+#     print("%s월은 겨울입니다" %mon)
+
+# if 4번 도 가능
