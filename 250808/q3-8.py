98f4779b0897a2d12f58cd50dd3f09c8a35432e8
sbg0629 <sbg0629@naver.com>
2025-08-08 19:19:14 +0900

    영문 입력받아서 맞는지 틀린지

---
+++ 250808/q3-8.py
@@ -0,0 +1,27 @@
+a = input("사자는 영어 단어오 무엇일까요")
+if a == "lion":
+    print("딩동댕 정답 입니다")
+else:
+    print("떙 틀렸습니다")
+
+a = input("오렌지는는 영어 단어오 무엇일까요")
+if a == "orange":
+    print("딩동댕 정답 입니다")
+else:
+    print("떙 틀렸습니다")
+
+a = input("기차는 영어 단어오 무엇일까요")
+if a == "train ":
+    print("딩동댕 정답 입니다")
+else:
+    print("떙 틀렸습니다")
+
+
+
+# 이렇게도 가능하다.
+# a = input("사자는 영어 단어오 무엇일까요")
+# result= "떙 틀렸습니다"
+# if a == "lion":
+#     result="딩동댕 맞았습니다~~"
+# print(result) 
+
