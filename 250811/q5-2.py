f4f5dab42eaa2db7143ba04b75a81f10001ce5fc
sbg0629 <sbg0629@naver.com>
2025-08-11 18:54:49 +0900

    5-1이랑 똑같은 결과 값 예제 리스트 만들기 while문 이용 업로드

---
+++ 250811/q5-2.py
@@ -0,0 +1,19 @@
+a = "i am a genius!"
+i = 0
+list=[]
+
+while i < len(a):
+    list.append(a[i])
+    i+=1
+print(list)
+
+
+
+
+
+
+# i = 0
+
+# while i < len(color):
+#     print(color[i])
+    # i+=1
