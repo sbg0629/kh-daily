96686574bed7909a3464f90801b987064b15b6d0
sbg0629 <sbg0629@naver.com>
2025-08-11 18:59:05 +0900

    반복문으로 key의 값에 갯수 출력

---
+++ 250811/q6-1.py
@@ -0,0 +1,10 @@
+person = {"name":"홍길동","age":30,"family":5,"children":["선미","성진","소영"],"pets":["강아지","고양이","이구아나"]}
+
+for key in person:
+    # print(key)
+    if key =="children":
+        # print(person[key])
+        # print(len(person[key]))
+        a = len(person[key])
+        print("자녀수 :",a)
+        
