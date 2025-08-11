c3c78e2b84712f6684f8e5280cf3630657c6cb1b
sbg0629 <sbg0629@naver.com>
2025-08-11 18:41:05 +0900

    find 함수 replace함수 split함수

---
+++ 250811/ex5-8.py
@@ -0,0 +1,16 @@
+string1 = "python is fun!"
+print(string1)
+
+# x = string1.find("fun")
+# print(x)
+
+# x = string1.replace("fun","nice") #fun이 nice로 바뀜
+# print(x)
+
+# list1 = string1.split(" ")#스페이스 기준으로 분리 되어 list로 바뀜
+# print(list1)
+# print(type(list1)) #<class 'list'>
+
+# for i in range(0,len(list1)):
+#     print("list1 [%d] : %s" %(i,list1[i]))
+
