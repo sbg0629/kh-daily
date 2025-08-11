d037063b3940d745780e3e2cad8b2554df665b99
sbg0629 <sbg0629@naver.com>
2025-08-11 18:40:26 +0900

    리스트안에 수 더하기 평균 구하기 reverse 함수 copy함수 sort 정렬 함수

---
+++ 250811/ex5-7.py
@@ -0,0 +1,20 @@
+scores = [80,90,85,95,100]
+
+# s = sum(scores)
+# avg = s/len(scores)
+
+# print("합계 = " , s) #전부 더한 값
+# print("평균 = " , avg)
+
+# scores.reverse() #리스트 뒤집기
+# print(scores)
+
+# x = scores.copy() #복사
+# print(x)
+# print(scores)
+
+scores.sort() #정렬
+print(scores)
+
+scores.sort(reverse=True) 
+print(scores)
