dfc19b6522fb8a2a20cb6e0af2ae9dca85f900a1
sbg0629 <sbg0629@naver.com>
2025-08-11 18:44:05 +0900

    튜플 합치기

---
+++ 250811/ex6-2.py
@@ -0,0 +1,17 @@
+# tup1=(10,20,30,40,50)
+tup1 = (10,30)
+tup2=(30,40)
+tup3 = tup1+tup2 #튜플 합치기
+
+# print(tup3)
+
+# for i in range(len(tup1)):
+#     print(tup1[i])
+
+admin_info = ("admin","12345","webmaset@naver.com")
+
+print("관리자 정보")
+print("아이디 : " + admin_info[0])
+print("비밀번호 : " + admin_info[1])
+print("이메일 : " + admin_info[2])
+
