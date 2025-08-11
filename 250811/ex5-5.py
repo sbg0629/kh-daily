873f8174626bc6de08b58770e2e4a55521b07cab
sbg0629 <sbg0629@naver.com>
2025-08-11 18:38:17 +0900

    리스트에 대한 insert문과 remove 함수

---
+++ 250811/ex5-5.py
@@ -0,0 +1,14 @@
+fruits = ["apple","orange","banana","cherry"]
+# print(fruits)
+
+# fruits.insert(1,"melon") #orange가 인덱스 1인데 orange 앞에 들어감
+# print(fruits)
+
+# fruits.insert(2,"strawberry") #orange가 인덱스 2인데 orange 앞에 들어감
+# print(fruits)
+
+# idx = fruits.index("banana") # 2 출력 index는 위치 반환
+# print(idx)
+
+fruits.remove("banana")
+print(fruits)
