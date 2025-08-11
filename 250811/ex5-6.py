83152324ce771ede1d3fd0ba09e5115c4a372d79
sbg0629 <sbg0629@naver.com>
2025-08-11 18:39:09 +0900

    리스트에 대한 pop 함수와 clear함수 와 리스트 +

---
+++ 250811/ex5-6.py
@@ -0,0 +1,15 @@
+fruits = ["apple","orange","banana","cherry"]
+fruits2 = ["mango","melon"]
+print(fruits)
+
+# x = fruits.pop(2) #리스트 요소 끄집어내기 (해당 인덱스 의 값을 잘라내기)
+# x = fruits.pop() #파라미터가 없으면 마지막 값
+# print(x)
+# print(fruits)
+
+# fruits.clear() #값을 다 지움
+# print(fruits)
+
+fr = fruits + fruits2 #리스트 더하기 
+print(fr) 
+
