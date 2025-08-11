5223776f2f18759fe0b590735d7cdc3095dd3f71
sbg0629 <sbg0629@naver.com>
2025-08-11 18:47:03 +0900

    함수 예제 시작

---
+++ 250811/ex7-1.py
@@ -0,0 +1,13 @@
+# 매개변수: 없음 반환값: 없음
+
+# ┌─함수 안에 색깔이 진하게 변험
+# def hello(name): #name : 매개변수 , 파라미터,아규먼트,인자값
+def hello(first_name,last_name): #name : 매개변수 , 파라미터,아규먼트,인자값
+    # print(" 안녕하세요!")
+    # print("%s 님 안녕하세요!" %name)
+    name = first_name+last_name
+    print("이름: ",name)
+# hello()
+hello("홍","지수")
+hello("안지","영")
+hello("황예")
