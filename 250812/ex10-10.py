29461e458fa6f4174c5994d0d12c0d629aaa6739
sbg0629 <sbg0629@naver.com>
2025-08-12 18:42:45 +0900

    트라이 캐치 문 예제

---
+++ 250812/ex10-10.py
@@ -0,0 +1,12 @@
+def divide(a,b):
+    try:
+        c=a/b
+    except ZeroDivisionError: #분모가 0일때 처리 
+        print("0 나누기 오류가 발생")
+    # finally:
+    else:
+        print(c)
+
+divide(30,10)
+divide(30,0)
+
