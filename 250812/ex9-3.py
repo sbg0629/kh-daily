46dd8e8ce7894aca7e1769891c7e265715ece4c2
sbg0629 <sbg0629@naver.com>
2025-08-12 18:39:13 +0900

    수학 모듈 사용

---
+++ 250812/ex9-3.py
@@ -0,0 +1,19 @@
+import math
+
+print("sin(2): ",math.sin(2))
+print("cos(2): ",math.cos(2))
+print("tan(2): ",math.tan(2))
+print("ceil(12.3): ",math.ceil(12.3)) #올림
+print("floor(12.3): ",math.floor(12.3)) #내림
+print("round(12.3): ",round(12.3)) #반올림
+
+print(math.fsum([1,2,3,4,5])) #리스트 합계
+print(math.fsum([1.3,2.5,3.2,4.4,5.2])) #리스트 실수 합계
+print(math.fsum([1.3,2.5,3.2,4.4,5.2])) #리스트  합계
+print(math.fsum((10,20,30))) #튜플의 합계
+print(math.fsum({10,20,3})) #딕셔너리의 합계
+
+print("log(75.3): ",math.log(75.3))#로그 값
+print("pow(5,3): ",math.pow(5,3))#거듭제곱 값
+print("sqrt(9): ",math.sqrt(9)) #제곱근 반환
+
