098a77338ba08cd3ccadc4c213e2db0d1c099382
sbg0629 <sbg0629@naver.com>
2025-08-12 18:38:58 +0900

    250812: calc.py import로 받아옴

---
+++ 250812/ex9-2.py
@@ -0,0 +1,30 @@
+# import calc
+
+# x = calc.add(10,20)
+# print(x)
+# y = calc.sub(20,5)
+# print(y)
+
+
+
+# import calc as c
+
+# x = c.add(10,20)
+# print(x)
+
+# y = c.sub(20,5)
+# print(y)
+
+
+
+
+
+# from calc import add
+from calc import add,sub
+
+x = add(10,20)
+print(x)
+
+y = sub(20,5)
+print(y)
+
